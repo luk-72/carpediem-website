074dd37a9a39ccddb7ff19111e9e941fc91c2fca
Luke <luca.d12366@gmail.com>
2020-12-25 23:03:25 +0100

    removed home

--- carpediem/carpediem/urls.py
+++ carpediem/carpediem/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('home.urls')),
+    path('', include('carpediem.views.index')),
     path('admin/', admin.site.urls),
 ]
