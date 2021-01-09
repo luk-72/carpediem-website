3fb884ff59e2b2d8de260149e7cf9d970dc40483
Luke <luca.d12366@gmail.com>
2021-01-09 17:24:35 +0100

    aggiunta admin

--- carpediem/carpediem/urls.py
+++ carpediem/carpediem/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from home import views
-
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     # ex: /polls/
+    path('admin/', admin.site.urls),
     path('', views.index, name='home'),
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- carpediem/home/models.py
+++ carpediem/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class imggal(models.Model):
+    imgtitle=models.CharField(max_length=100)
+    imgdesc=models.CharField(max_length=500)
+    image=models.ImageField(upload_to='static\img\portfolio/')
+   
